3b7aa4a8e04566c83c71b5f676294ae8dcec9043
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-09-06 17:02:40 +0200

    esercizio-base

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ createApp({
   data() {
     return {
       randomMail: "",
+      arrayRandomMail: [],
     };
   },
 
@@ -11,10 +12,9 @@ createApp({
     for (let index = 0; index < 10; index++) {
       axios
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(function (mail) {
+        .then((mail) => {
           const result = mail.data.response;
-          this.randomMail = result;
-          console.log(result);
+          this.arrayRandomMail.push(result);
         });
     }
   },
